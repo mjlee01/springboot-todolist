1ddd99901ff3bf1fd275c1e2a989d022a3e7fb8a
Lee Ming Jia <lmj11083@gmail.com>
2025-09-06 19:30:54 +0800

    Edited api route

--- src/main/java/com/dave/todolist/controller/TodoController.java
+++ src/main/java/com/dave/todolist/controller/TodoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/todo")
+@RequestMapping(value = "/to-do")
 public class TodoController {
     @Autowired
     private TodoRepo todoRepo;
